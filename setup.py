8442853df4dcf868b8ae707d26ff36f0d2ed3d56
speed785 <speed785@users.noreply.github.com>
2026-03-05 23:17:18 +0100

    chore: add setup.py extras for llamaindex and crewai

--- setup.py
+++ setup.py
@@ -1,3 +1,8 @@
 from setuptools import setup  # pyright: ignore[reportMissingModuleSource]
 
-_ = setup()
+_ = setup(
+    extras_require={
+        "llamaindex": ["llama-index-core>=0.10.0"],
+        "crewai": ["crewai>=0.28.0"],
+    }
+)
